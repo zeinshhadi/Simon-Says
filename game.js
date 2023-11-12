68260607598f0c6ff65ebe067e7c1a85646152f4
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-12 16:44:29 +0200

    removed start button ane make it click anything to start

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
 let computerSequence = [];
 let playerSequence = [];
-//   for (let i = 0; i <= computerSequence.length; i++) {}
 let greenButton = document.getElementById("green").addEventListener("click", playerClick);
 let redButton = document.getElementById("red").addEventListener("click", playerClick);
 let yellowButton = document.getElementById("yellow").addEventListener("click", playerClick);
 let blueButton = document.getElementById("blue").addEventListener("click", playerClick);
-let startButton = document.getElementById("start").addEventListener("click", startGame);
+let startButton = document.body.addEventListener("keydown", startGame);
 
 function playAudio(color) {
    let audio = new Audio(`sounds/${color}.mp3`);
-
    audio.play();
 }
 function flashButton(currentButton) {
@@ -40,13 +38,11 @@ function addToSequence() {
 }
 
 function startComputerSequence() {
-   console.log(`computer sequence = ${computerSequence}`);
-   console.log(`player sequence = ${playerSequence}`);
    for (let i = 0; i < computerSequence.length; i++) {
       setTimeout(() => {
          flashButton(computerSequence[i]);
          playAudio(getColorById(computerSequence[i]));
-      }, i * 1000);
+      }, i * 500);
    }
 }
 
@@ -56,41 +52,39 @@ function playerClick(event) {
       flashButton(1);
       playAudio(getColorById(1));
       playerSequence.push(1);
-      checkSequence(1);
-      console.log(typeof 1);
    } else if (colorSelected == "red") {
       flashButton(2);
       playAudio(getColorById(2));
       playerSequence.push(2);
-      checkSequence(2);
    } else if (colorSelected == "yellow") {
       flashButton(3);
       playAudio(getColorById(3));
       playerSequence.push(3);
-      checkSequence(3);
    } else {
       flashButton(4);
       playAudio(getColorById(4));
       playerSequence.push(4);
-      checkSequence(4);
    }
-   console.log(`player sequence = ${playerSequence}`);
+   checkSequence();
 }
 
 function checkSequence() {
    for (let i = 0; i < playerSequence.length; i++) {
       if (playerSequence[i] !== computerSequence[i]) {
          playAudio("wrong");
-         console.log(`wrong interrupt`);
+         document.body.style.backgroundColor = "red";
+         setTimeout(() => {
+            document.body.style.backgroundColor = " #011F3F";
+         }, 500);
 
          reset();
+
          return;
       }
    }
    if (playerSequence.length === computerSequence.length) {
       playerSequence = [];
       setTimeout(() => {
-         console.log(`correct interrupt`);
          addToSequence();
       }, 1500);
    }
@@ -100,10 +94,12 @@ function reset() {
    computerSequence = [];
    playerSequence = [];
    setTimeout(() => {
-      startComputerSequence();
+      addToSequence();
    }, 1500);
 }
 
 function startGame() {
-   addToSequence();
+   if (computerSequence.length == 0) {
+      addToSequence();
+   }
 }
