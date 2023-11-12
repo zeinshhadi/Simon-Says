ed6b970296dd509a36968a7e3337787aa831aaf9
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-12 14:44:43 +0200

    added user turn but with start button

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let greenButton = document.getElementById("green").addEventListener("click", pla
 let redButton = document.getElementById("red").addEventListener("click", playerClick);
 let yellowButton = document.getElementById("yellow").addEventListener("click", playerClick);
 let blueButton = document.getElementById("blue").addEventListener("click", playerClick);
+let startButton = document.getElementById("start").addEventListener("click", startGame);
 
 function playAudio(color) {
    let audio = new Audio(`sounds/${color}.mp3`);
@@ -33,11 +34,14 @@ function getColorById(id) {
 
 function addToSequence() {
    let newNumberToAdd = Math.floor(Math.random() * 4) + 1;
+
    computerSequence.push(newNumberToAdd);
    startComputerSequence();
 }
 
 function startComputerSequence() {
+   console.log(`computer sequence = ${computerSequence}`);
+   console.log(`player sequence = ${playerSequence}`);
    for (let i = 0; i < computerSequence.length; i++) {
       setTimeout(() => {
          flashButton(computerSequence[i]);
@@ -46,8 +50,60 @@ function startComputerSequence() {
    }
 }
 
-function playerClick() {
-   flashButton(1);
-   playAudio(getColorById(1));
+function playerClick(event) {
+   let colorSelected = event.target.id;
+   if (colorSelected == "green") {
+      flashButton(1);
+      playAudio(getColorById(1));
+      playerSequence.push(1);
+      checkSequence(1);
+      console.log(typeof 1);
+   } else if (colorSelected == "red") {
+      flashButton(2);
+      playAudio(getColorById(2));
+      playerSequence.push(2);
+      checkSequence(2);
+   } else if (colorSelected == "yellow") {
+      flashButton(3);
+      playAudio(getColorById(3));
+      playerSequence.push(3);
+      checkSequence(3);
+   } else {
+      flashButton(4);
+      playAudio(getColorById(4));
+      playerSequence.push(4);
+      checkSequence(4);
+   }
+   console.log(`player sequence = ${playerSequence}`);
 }
-addToSequence();
+
+function checkSequence() {
+   for (let i = 0; i < playerSequence.length; i++) {
+      if (playerSequence[i] !== computerSequence[i]) {
+         playAudio("wrong");
+         console.log(`wrong interrupt`);
+
+         reset();
+         return;
+      }
+   }
+   if (playerSequence.length === computerSequence.length) {
+      playerSequence = [];
+      setTimeout(() => {
+         console.log(`correct interrupt`);
+         addToSequence();
+      }, 1500);
+   }
+}
+
+function reset() {
+   computerSequence = [];
+   playerSequence = [];
+   setTimeout(() => {
+      startComputerSequence();
+   }, 1500);
+}
+
+function startGame() {
+   addToSequence();
+}
