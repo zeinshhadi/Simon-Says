f81368c21c73b08da49c04f6a5ce399d044fb12a
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-13 13:47:51 +0200

    disable and enable btn on each turn + comments

--- game.js
+++ game.js
@@ -1,22 +1,12 @@
+// intializing all variables we need accors the script
 let computerSequence = [];
 let playerSequence = [];
-let greenButton = document.getElementById("green").addEventListener("click", playerClick);
-let redButton = document.getElementById("red").addEventListener("click", playerClick);
-let yellowButton = document.getElementById("yellow").addEventListener("click", playerClick);
-let blueButton = document.getElementById("blue").addEventListener("click", playerClick);
-let startButton = document.body.addEventListener("keydown", startGame);
-
-function playAudio(color) {
-   let audio = new Audio(`sounds/${color}.mp3`);
-   audio.play();
-}
-function flashButton(currentButton) {
-   document.getElementById(getColorById(currentButton)).classList.add("pressed");
-   setTimeout(() => {
-      document.getElementById(getColorById(currentButton)).classList.remove("pressed");
-   }, 500);
-}
-
+const greenButton = document.getElementById("green").addEventListener("click", playerClick);
+const redButton = document.getElementById("red").addEventListener("click", playerClick);
+const yellowButton = document.getElementById("yellow").addEventListener("click", playerClick);
+const blueButton = document.getElementById("blue").addEventListener("click", playerClick);
+const startButton = document.body.addEventListener("keydown", startGame);
+//A function to set an id for each color so we can got the color in return according to id recieved
 function getColorById(id) {
    switch (id) {
       case 1:
@@ -29,6 +19,23 @@ function getColorById(id) {
          return "blue";
    }
 }
+//playing audio according to the color recieved
+
+function playAudio(color) {
+   let audio = new Audio(`sounds/${color}.mp3`);
+   audio.play();
+}
+
+//flashing button used
+
+function flashButton(currentButton) {
+   document.getElementById(getColorById(currentButton)).classList.add("pressed");
+   setTimeout(() => {
+      document.getElementById(getColorById(currentButton)).classList.remove("pressed");
+   }, 500);
+}
+
+//Adding a new random number between 1 and 4 to computerSequence array and calling it to start
 
 function addToSequence() {
    let newNumberToAdd = Math.floor(Math.random() * 4) + 1;
@@ -37,15 +44,24 @@ function addToSequence() {
    startComputerSequence();
 }
 
+//Computer turn started
+
 function startComputerSequence() {
+   disableButtonWhenComputerTurn(); //function to disable the click listener of the user when computer turn is running its own turn
    for (let i = 0; i < computerSequence.length; i++) {
       setTimeout(() => {
          flashButton(computerSequence[i]);
          playAudio(getColorById(computerSequence[i]));
-      }, i * 500);
+      }, i * 1000);
+      setInterval(() => {}, computerSequence.length * 1000);
    }
+   setTimeout(() => {
+      enableButtonWhenPlayerTurn(); //function to enable the click listener of the user when computer turn is over
+   }, computerSequence.length * 800);
 }
 
+//user turn start whenever he click and event is loaded and sent to its corresponding functions with the id according to color
+//by getting the id of the event click which in this case the name of the color as srting
 function playerClick(event) {
    let colorSelected = event.target.id;
    if (colorSelected == "green") {
@@ -65,12 +81,16 @@ function playerClick(event) {
       playAudio(getColorById(4));
       playerSequence.push(4);
    }
+   //calling checksequence function will check the user sequence with the computer sequence
    checkSequence();
 }
+
+//check sequence will check user input one by one with computer sequence if one dont match is a loss and game will reset,else will call add sequence and continue
 
 function checkSequence() {
    for (let i = 0; i < playerSequence.length; i++) {
       if (playerSequence[i] !== computerSequence[i]) {
+         playerSequence[i];
          playAudio("wrong");
          document.body.style.backgroundColor = "red";
          setTimeout(() => {
@@ -83,23 +103,39 @@ function checkSequence() {
       }
    }
    if (playerSequence.length === computerSequence.length) {
+      disableButtonWhenComputerTurn();
       playerSequence = [];
       setTimeout(() => {
          addToSequence();
       }, 1500);
    }
 }
-
+//reset function to reset all thing and restart game when you lose
 function reset() {
+   disableButtonWhenComputerTurn();
    computerSequence = [];
    playerSequence = [];
    setTimeout(() => {
       addToSequence();
    }, 1500);
 }
-
+//starting the game after pressing and key on you keyboard
 function startGame() {
    if (computerSequence.length == 0) {
       addToSequence();
    }
+}
+//function that disables event listeners
+function disableButtonWhenComputerTurn() {
+   let buttons = document.querySelectorAll(".btn");
+   buttons.forEach((button) => {
+      button.removeEventListener("click", playerClick);
+   });
+}
+//function that enables event listeners
+function enableButtonWhenPlayerTurn() {
+   let buttons = document.querySelectorAll(".btn");
+   buttons.forEach((button) => {
+      button.addEventListener("click", playerClick);
+   });
 }
